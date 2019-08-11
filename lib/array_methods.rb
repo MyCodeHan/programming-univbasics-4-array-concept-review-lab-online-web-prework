ba19ce8b7f298af4ee0d65ccf86992c1fb5d5a2a
Hannah Harrington <hannahharrington@outlook.com>
2019-08-11 23:37:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,7 @@
 
 
 def find_element_index(array, value_to_find)
-      expect(find_element_index(scale,2))
-      expect(find_element_index(scale,4))
-      expect(find_element_index(scale_2,5))
-      expect(find_element_index(scale_2,6))
+     
 end
 
 def find_max_value(array)
